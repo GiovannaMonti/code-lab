11c41a6eaa340a42cbfe08038c497c6c787c062e
Giovanna Monti <ladyarcadia98@live.it>
2022-03-28 17:30:12 +0200

    add simple page transition with barbajs

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
+import barba from "@barba/core"
+import gsap from "gsap"
+
+/* let mouseX = 0
+let mouseY = 0
+document.addEventListener("click", (e) => {
+  mouseX = e.clientX
+  mouseY = e.clientY
+  console.log(mouseX, mouseY)
+})
+ */
+barba.init({
+  transitions: [
+    {
+      name: "opacity",
+      enter(data) {
+        // create your stunning enter animation here
+        return gsap.from(data.next.container, {
+          autoAlpha: 0,
+          duration: 0.3,
+        })
+      },
+      leave(data) {
+        // create your stunning leave animation here
+        return gsap.to(data.current.container, {
+          autoAlpha: 0,
+          duration: 0.3,
+          display: "none",
+        })
+      },
+    },
+  ],
+})
+
 const menu = document.getElementById("open-menu")
 
 menu.addEventListener("click", () => {
